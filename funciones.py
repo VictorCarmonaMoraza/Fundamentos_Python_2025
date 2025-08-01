cb7a2c9132f7e86c38908134541d6a3f133831ed
Victor Carmona <victorcarmona9035.com>
2025-08-01 13:50:58 +0200

    funciones en python con la libreria math

---
+++ funciones.py
@@ -0,0 +1,18 @@
+
+import math
+
+##Redonde dear un número a una cifra decimal
+numero1 = round(3.18,1)  # Output: 3.2
+print(numero1)
+
+##Truncar un numero
+numero2 = 3.18
+print(math.trunc(numero2))
+
+##Redondeo hacia arriba
+numero3 = 3.56
+print(math.ceil(numero3))
+
+##Redondeo hacia abajo
+numero4 = 3.56
+print(math.floor(numero4))
